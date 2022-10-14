18c089983ab3c6c5d15971adf70a73c1fd2cea97
unknown <sarmad.iict@gmail.com>
2022-10-15 02:53:39 +0500

    title changed

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Title Page</title>
+        <title>Login Page</title>
 
         <!-- Bootstrap CSS -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
